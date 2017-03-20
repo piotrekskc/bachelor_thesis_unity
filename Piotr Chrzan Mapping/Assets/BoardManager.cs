511d45268693e54783789d1ab6aa12606cccf88e
Piotrek <piotrek.chrzan@gmail.com>
2017-03-20 10:51:24 +0100

    added Turn Based Strategy Framework. Not connected. https://www.assetstore.unity3d.com/en/#!/content/50282

---
+++ Piotr Chrzan Mapping/Assets/BoardManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random; //Tells Random to use the Unity Engine random number generator
+
+public class BoardManager : MonoBehaviour {
+
+    public int NumberOfTurns = 10;
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
