f3f1d8ce3da21be06b2377b84144d122bde46135
Piotrek <piotrek.chrzan@gmail.com>
2017-04-27 15:59:19 +0200

    small changes, small scripts
    Former-commit-id: 9985865307ecebe056e8d894bd839b774d35bf51

---
+++ Piotr Chrzan Mapping/Assets/activityScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activityScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public GameObject shermanIcon;
+    public GameObject PanelSherman;
+	// Update is called once per frame
+	void Update () {
+
+
+		if(shermanIcon.activeSelf)
+        {
+            PanelSherman.SetActive(false);
+        }
+
+
+
+	}
+}
