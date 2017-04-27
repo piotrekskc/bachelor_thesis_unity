a26b9315bf2de663f8a774f23fd59eb1747d92ce
Piotrek <piotrek.chrzan@gmail.com>
2017-04-27 15:45:11 +0200

    Everything cleaned up. Deleted TBS Framework, generated my grid with colliders.
    Former-commit-id: 9a9665d13a7478e9d81297d5b3803fd67d9263a1

---
+++ Piotr Chrzan Mapping/Assets/MyScripts/GridGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+    public int GridWidth;
+    public int GridLength;
+
+    public GameObject prefabCube;
+    public void generateGrid()
+    {
+        Vector3 InitialPosition = new Vector3(0, 0, 0);
+
+        for(int x = 0;x<GridWidth;x++)
+        {
+            for(int z =0;z<GridLength;z++)
+            {
+                Instantiate(prefabCube, new Vector3(x /10f, 0, z/10f), Quaternion.identity);
+
+
+            }
+        }
+    }
+	// Use this for initialization
+	void Start () {
+
+
+        generateGrid();
+        
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Piotr Chrzan Mapping/Assets/UnitStats.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour {
+    public double HP;
+    public double AttackRange;
+    public float MovementRange;
+    public double AttackStrength;
+    public double Defence;
+    public double DefenceFactor;
+    public double FrontArmour;
+    public double SideArmour;
+    public double BackArmour;
+
+    public bool isChoosen = false;
+    public bool isDead = false;
+    public bool nonActive = false;
+
+    public GameObject Unit;
+    public Texture2D tex;
+    private Sprite mySprite;
+    public void DrawAreaofMove()
+    {
+      //  GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+       // cube.transform.position = new Vector3(Unit.transform.position.x-(MovementRange/20), 0.01F, Unit.transform.position.z - (MovementRange / 20));
+        mySprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+    }
+
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+        sr.color = new Color(0.9f, 0.9f, 0.9f, 1.0f);
+
+        transform.position = new Vector3(1.5f, 1.5f, 0.0f);
+    }
+
+    // Use this for initialization
+    void Start () {
+        DrawAreaofMove();
+
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 100), "Add sprite"))
+        {
+            sr.sprite = mySprite;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    
+    }
