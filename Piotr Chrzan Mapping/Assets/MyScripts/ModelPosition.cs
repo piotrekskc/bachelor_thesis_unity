512e9389a844a326b2ba408667a776093822c666
piotrekskc <piotrek.chrzan@gmail.com>
2017-05-04 15:32:43 +0200

    Revert "UI dziala jeszcze pare niedociagniec. wykrywanie markerow jest na razie w stanie beznadziejnym."
    This reverts commit 31d21a68900e755b3f6799374714833886e410bb [formerly 9c696922875a843f5cac6b5972c7f09fde72fa84].
    
    
    Former-commit-id: d614d2b47a5d8bd0815af386ddf92c617cfc87ad

--- Piotr Chrzan Mapping/Assets/MyScripts/ModelPosition.cs
+++ Piotr Chrzan Mapping/Assets/MyScripts/ModelPosition.cs
@@ -17,10 +17,11 @@ public class ModelPosition : MonoBehaviour {
 
         var pos = marker.transform.position;
        // var cornerPos = Rcorner.transform.position;
-        
+        float xpos = pos.x / 0.7056f;
+        float zpos = pos.z / 0.7056f;
         var rot = marker.transform.rotation;
         var rotm = model.transform.rotation;
-        model.transform.position = new Vector3(marker.transform.position.x, 0.01f, marker.transform.position.z);
+        model.transform.position = new Vector3(xpos, 0.01f, zpos);
         model.transform.rotation = new Quaternion(rotm.x, rot.y, rotm.z,rotm.w);
     }
 }
